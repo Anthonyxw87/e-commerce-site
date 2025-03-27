47a59ce4438193147f2d835382a946236d9089b4
Anthony Wang <anthonywang64@gmail.com>
2025-03-27 15:58:28 -0400

    update correct ports to docker deploy scripts #2

--- scripts/deploy-backend.sh
+++ scripts/deploy-backend.sh
@@ -36,7 +36,7 @@ if [ "$CONTAINER_EXISTS" = false ] || [ "$(docker inspect --format='{{.Id}}' $IM
     docker rm "$CONTAINER_NAME" >> "$LOG_FILE" 2>&1
   fi
 
-  docker run -d --name "$CONTAINER_NAME" -p 5000:5000 "$IMAGE_NAME" >> "$LOG_FILE" 2>&1
+  docker run -d --name "$CONTAINER_NAME" -p 5001:5001 "$IMAGE_NAME" >> "$LOG_FILE" 2>&1
 else
   echo "[$(date)] Image is up to date. No changes." >> "$LOG_FILE"
 fi
