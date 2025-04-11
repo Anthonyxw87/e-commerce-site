fe5e042a3b57b64abdc26078f210a202a0c90d6d
Anthony Wang <anthonywang64@gmail.com>
2025-04-11 10:16:26 -0400

    fix directories for frontend; delete mistakes

--- frontend/src/routes/user_routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, jsonify
-from utils.auth import requires_auth
-
-user_bp = Blueprint("user", __name__)
-
-@user_bp.route("/profile", methods=["GET"])
-@requires_auth
-def profile():
-    return jsonify({"message": "You are authenticated!"})
