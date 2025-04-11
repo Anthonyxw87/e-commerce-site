fad71a8ee8cc756dcbb002ebfbbffc88c024bfd8
Anthony Wang <anthonywang64@gmail.com>
2025-04-11 07:48:49 -0400

    update frontend and backend directory structure to be more advanced

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,10 @@
-import './App.css';
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload...
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello
     </div>
   );
 }
